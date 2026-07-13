dd3005d5d2056da38e1cdf5e107b1c39eb67cdbb
Kevin Harding, MIS <146769989+KHard2Bme@users.noreply.github.com>
2026-07-13 09:27:06 -0400

    update

--- ubuntu_updates.sh
+++ ubuntu_updates.sh
@@ -26,16 +26,28 @@ echo "======================================"
 sudo apt-get install -y apache2
 
 echo "======================================"
+echo "Enabling Apache2..."
+echo "======================================"
+
+sudo systemctl enable apache2
+
+echo "======================================"
 echo "Starting Apache2..."
 echo "======================================"
 
-sudo service apache2 start
+sudo systemctl start apache2
 
 echo "======================================"
 echo "Apache2 Status"
 echo "======================================"
 
 sudo systemctl status apache2 --no-pager
+
+echo "======================================"
+echo "Apache Version"
+echo "======================================"
+
+apache2 -v
 
 echo "======================================"
 echo "Script Complete!"
