a5683dbb72ceb159b7f85691276dee49ec135869
Kevin Harding, MIS <146769989+KHard2Bme@users.noreply.github.com>
2026-07-13 06:18:17 -0400

    update

--- outputs.tf
+++ outputs.tf
@@ -13,10 +13,6 @@ output "instance_names" {
   value = aws_instance.swarm[*].tags.Name
 }
 
-#################################################
-# CloudWatch Dashboard
-#################################################
-
 output "cloudwatch_dashboard" {
 
   value = aws_cloudwatch_dashboard.docker_swarm.dashboard_name
--- variables.tf
+++ variables.tf
@@ -24,10 +24,6 @@ variable "public_subnet" {
   default = "10.0.1.0/24"
 }
 
-#################################################
-# CloudWatch Dashboard
-#################################################
-
 variable "dashboard_name" {
   description = "CloudWatch Dashboard Name"
   type        = string
