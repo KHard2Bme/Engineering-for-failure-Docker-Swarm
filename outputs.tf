77bebab041f895bf988f104a35dbc6592af49336
Kevin Harding, MIS <146769989+KHard2Bme@users.noreply.github.com>
2026-07-21 06:38:50 -0400

    update

--- outputs.tf
+++ outputs.tf
@@ -46,3 +46,39 @@ output "cloudwatch_dashboard_name" {
   description = "CloudWatch Dashboard"
   value       = aws_cloudwatch_dashboard.dashboard.dashboard_name
 }
+
+output "cloudwatch_log_group_name" {
+  description = "CloudWatch Log Group"
+  value       = aws_cloudwatch_log_group.docker_logs.name
+}
+
+output "cloudwatch_log_group_arn" {
+  description = "CloudWatch Log Group ARN"
+  value       = aws_cloudwatch_log_group.docker_logs.arn
+}
+
+output "cloudwatch_iam_role_name" {
+  description = "IAM Role used by the CloudWatch Agent"
+  value       = aws_iam_role.cloudwatch_agent_role.name
+}
+
+output "cloudwatch_instance_profile_name" {
+  description = "IAM Instance Profile for EC2"
+  value       = aws_iam_instance_profile.cloudwatch_agent_profile.name
+}
+
+output "container_failure_metric" {
+  description = "Container failure metric filter"
+  value       = aws_cloudwatch_log_metric_filter.container_failure.name
+}
+
+output "worker_failure_metric" {
+  description = "Worker node failure metric filter"
+  value       = aws_cloudwatch_log_metric_filter.worker_failure.name
+}
+
+output "manager_failure_metric" {
+  description = "Manager node failure metric filter"
+  value       = aws_cloudwatch_log_metric_filter.manager_failure.name
+}
+
